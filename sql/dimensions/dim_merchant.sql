bb8fd390c1c086517b350ccecc9814e4acdaaecb
gersonarroyo <gerson_de@protonmail.com>
2026-05-28 12:30:47 -0300

    fixing psql and pgadmin configurations

--- sql/dimensions/dim_merchant.sql
+++ sql/dimensions/dim_merchant.sql
@@ -1,7 +1,8 @@
 INSERT INTO dw.dimMerchant (merchant_name, category, merch_lat, merch_long)
-SELECT DISTINCT
+SELECT DISTINCT ON (merchant)
     merchant as merchant_name,
     category,
     merch_lat::decimal(10,6),
     merch_long::decimal(10,6)
-FROM staging.transactions;
+FROM staging.transactions
+ORDER BY merchant;
